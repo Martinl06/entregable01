bd04ab01d8ca5a6adeee39a88d1068da2a5a2286
Martin Lujan <martinlujan0666@gmail.com>
2023-04-11 02:04:26 -0300

    entrega01

--- index.js
+++ index.js
@@ -1,47 +1,65 @@
 
-class ProductManager{
+class Product {
+    constructor(title, description, price, thumbnail, code, stock) {
+        this.id = 0
+        this.title = title
+        this.description = description
+        this.price = price
+        this.thumbnail = thumbnail
+        this.code = code
+        this.stock = stock
+    }
+}
+
+class ProductManager {
     constructor(){
         this.products = []
-        this.lastProductID = 0
     }
 
-    addProduct(product){
-        if(!product.Title || !product.Description || !product.Price || !product.Code || !product.thumbnail || !product.stock){
-            console.log("Todos los campos son obligatorios")
-        }
-        if(this.products.some(product => product.Code === product.Code)){
-            console.log("El producto ya existe")
-        }
 
-
-        this.lastProductID
-        const newProduct = {
-            Title: product.Title,
-            Description: product.Description,
-            Price: product.Price,
-            Code: product.Code,
-            thumbnail: product.thumbnail,
-            stock: product.stock,
-        }
-
-        this.products.push(newProduct)
-        console.log("Producto agregado al carrito", newProduct)
+ addProduct = (title, description, price, thumbnail, code, stock) => {
+    if(!title || !description || !price || !thumbnail || !code || !stock){
+        console.log("Los campos son obligatorios")
+        return
     }
 
-    getProducts(){
-        return this.products
-
+    if(this.products.find(product =>product.code === code)){
+        console.log("El codigo del producto ya esta en uso")
+        return
     }
 
-    getProductById(id){
-        const product = this.product.find(product => product.id === id)
-        if(!product){
-            console.log("Not found")
-        }
-        return product
-    }
-    
-    
+    const id = this.products.length + 1
+    const product = new Product(title, description, price, thumbnail, code, stock)
+    product.id = id
+    this.products.push(product)
+    console.log("Producto agregado con exito")
 }
 
 
+ getProducts = () => {
+    return this.products
+}
+
+ getProductById = (id) => {
+    const product = this.products.find(product => product.id === id)
+        if(product){
+            return product
+        }else{
+            console.log("Producto no encontrado")
+        }
+    }
+}
+
+const productManager = new ProductManager()
+
+console.log(productManager.getProducts())
+
+productManager.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25)
+
+console.log(productManager.getProducts())
+
+productManager.addProduct("producto prueba2", "Este es un producto prueba2", 100, "Sin imagen", "abc123", 2)
+
+console.log(productManager.getProductById(1))
+
+console.log(productManager.getProductById(2))
